1b1095149800066f359106416b666fe6815f018f
Ash Khanna <ashkhanna@Ashs-MacBook-Pro.local>
2026-05-13 21:41:31 -0400

    v6.9599999999998799029090101991090114

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/App1.jsx', 'src/App2.jsx']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -16,6 +16,16 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      'no-useless-assignment': 'off',
+      'no-empty': 'off',
+      'react-hooks/refs': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/immutability': 'off',
+      'react-hooks/purity': 'off',
+      'react-hooks/static-components': 'off',
     },
   },
 ])
